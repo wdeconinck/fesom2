71bf0b975e8f7e0045033080fa9f1114aebbfce3
Willem Deconinck <willem.deconinck@ecmwf.int>
2023-08-24 15:28:54 +0200

    Update GHA

--- configure.sh
+++ configure.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 set -e
-
+HERE=$PWD
 source env.sh # source this from your run script too
-mkdir build || true # make sure not to commit this to svn or git
+mkdir -p build
 cd build
-cmake .. # not required when re-compiling
+cmake .. -DCMAKE_INSTALL_PREFIX=$HERE ${CMAKE_ARGS}
 make install -j`nproc --all`
